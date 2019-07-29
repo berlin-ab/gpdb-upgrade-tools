1d6eea7e83c206ea52e2575f3e910252dc226774
Adam Berlin <aberlin@pivotal.io>
2019-07-29 17:48:23 -0400

    Initial commit of partition checks
    - check to see if a partition table leaf node has a differing partition rule than the root

---
+++ partition-check.sql
@@ -0,0 +1,59 @@
+drop function if exists find_leaf_partitions();
+drop function if exists find_leaves_with_root_oid();
+drop function if exists find_leaf_policies_with_root_policies();
+drop function if exists find_leaf_partitions_with_mismatching_policies_to_root(text);
+drop function if exists policy(regclass);
+drop function if exists find_distribution_for_table(regclass);
+
+
+create function find_leaf_partitions() returns table (leaf_table regclass, parent_tuple_oid regclass) as $$
+	select parchildrelid, paroid from pg_partition_rule where paroid in (
+		select oid from pg_partition outer_pg_partition WHERE parlevel = (
+			select max(parlevel) from pg_partition
+			where pg_partition.parrelid = outer_pg_partition.parrelid
+		)
+	)
+$$ language sql;
+
+
+create function find_leaves_with_root_oid() returns table (parent_oid regclass, leaf_table regclass) as $$
+	select pg_partition.parrelid, leaves.leaf_table from find_leaf_partitions() as leaves
+		inner join pg_partition on leaves.parent_tuple_oid = pg_partition.oid
+$$ language sql;
+
+
+create function find_leaf_policies_with_root_policies() returns table (
+	parent_oid regclass, leaf_table regclass, root_localoid regclass, root_attrnums smallint[], leaf_localoid regclass, leaf_attrnums smallint[]
+	) as $$
+	select * from find_leaves_with_root_oid() as leaves
+		inner join gp_distribution_policy parent_policy on parent_policy.localoid = leaves.parent_oid
+		inner join gp_distribution_policy child_policy on child_policy.localoid = leaves.leaf_table
+$$ language sql;
+
+
+
+create function find_leaf_partitions_with_mismatching_policies_to_root(schema_name text) returns table(
+	leaf_table regclass,
+	root_table regclass
+) as $$
+	select leaf_table, root_localoid from
+		find_leaf_policies_with_root_policies() as all_leaves
+		inner join pg_namespace on pg_namespace.nspname = $1
+		inner join pg_class on (pg_class.oid = leaf_table and pg_class.relnamespace = pg_namespace.oid)
+		where coalesce(all_leaves.root_attrnums, cast('{}' as smallint[])) !=
+			coalesce(all_leaves.leaf_attrnums, cast('{}' as smallint[]));
+$$ language sql;
+
+
+create function policy(table_oid regclass) returns table(attnum smallint) as $$
+	select unnest(attrnums) from gp_distribution_policy where localoid = $1;
+$$ language sql;
+
+
+create function find_distribution_for_table(some_table regclass) returns name[] as $$
+	select array_agg(attname order by row_number) attributes
+		from pg_attribute join (
+			select attnum, row_number() over() from policy($1)
+		) t(attnum, row_number) on pg_attribute.attnum = t.attnum
+		where attrelid = $1;
+$$ language sql;
---
+++ test/partition_check_test.py
@@ -0,0 +1,135 @@
+import unittest
+import sqlalchemy
+
+
+class PartitionCheckTest(unittest.TestCase):
+    def setup_database_connection(self):
+        from sqlalchemy import create_engine
+        db_string = "postgresql://localhost:5000/adamberlin"
+        self.engine = create_engine(db_string)
+        self.db = self.engine.connect()
+
+    def create_self_contained_schema(self, schema_name):
+        self.db.execute("""
+            drop schema if exists {schema_name} cascade;
+            create schema {schema_name};
+        """.format(schema_name=schema_name))
+
+    def use_schema(self, schema_name):
+        self.db.execute("""
+            set search_path to {schema_name};
+        """.format(schema_name=schema_name))
+
+    def setUp(self):
+        self.setup_database_connection()
+
+    def tearDown(self):
+        self.db.close()
+
+    def create_table_with_multilevel_partition(self):
+        self.db.execute("""
+            drop table if exists example_table;
+            create table example_table (
+                b int,
+                a int,
+                c int
+            )
+            distributed by (a)
+            partition by range (a)
+            subpartition by range (b)
+            subpartition by range (c)
+            (
+                partition ein start (0) end (10) ( -- a
+                    subpartition partition_a start (0) end(5) ( -- b
+                        subpartition partition_c start (0) end (3), -- c
+                        subpartition partition_d start (3) end (5) -- c
+                    ),
+                    subpartition partition_b start (5) end(10) ( -- b
+                        subpartition partition_e start(5) end (10) -- c
+                    )
+                ),
+                partition zwei start (10) end (20) ( -- a
+                    subpartition partition_z start (10) end (20) ( -- b
+                        subpartition partition_y start (0) end (3) -- c
+                    )
+                )
+            );
+        """)
+
+    def import_partition_check_library(self):
+        import os
+        script_dir = os.path.dirname(__file__)
+        library_path = os.path.join(script_dir, "..", "partition-check.sql")
+        self.db.execute(open(library_path).read())
+
+    def alter_leaf_node_to_have_a_different_policy(self):
+        self.db.execute("""
+            alter table example_table_1_prt_ein_2_prt_partition_a_3_prt_partition_c set distributed randomly;
+        """)
+
+    def exchange_a_valid_table_into_the_partition_hierarchy_with_alternate_attribute_ordering(self):
+        self.db.execute("""
+            drop table if exists table_to_exchange;
+            
+            create table table_to_exchange (
+                x int,
+                b int,
+                a int,
+                c int
+            ) distributed by (a);
+            
+            alter table table_to_exchange drop column x;
+            
+            alter table example_table
+                alter partition ein
+                alter partition partition_a
+                exchange partition partition_c
+                with table table_to_exchange; 
+        """)
+
+    def test_partitions_with_mismatching_policies_are_returned_for_the_given_schema(self):
+        self.create_self_contained_schema('myschema')
+        self.use_schema('myschema')
+        self.import_partition_check_library()
+
+        self.create_table_with_multilevel_partition()
+        self.alter_leaf_node_to_have_a_different_policy()
+
+        rows = self.db.execute("""
+            select * from find_leaf_partitions_with_mismatching_policies_to_root('myschema');
+        """).fetchall()
+
+        self.assertEqual(len(rows), 1)
+
+        mismatching_result = rows[0]
+        self.assertEqual(mismatching_result['leaf_table'], 'example_table_1_prt_ein_2_prt_partition_a_3_prt_partition_c')
+        self.assertEqual(mismatching_result['root_table'], 'example_table')
+
+    def test_no_mismatching_partitions_are_returned_for_a_different_schema(self):
+        self.create_self_contained_schema('myschema')
+        self.create_self_contained_schema('myotherschema')
+        self.use_schema('myotherschema')
+        self.import_partition_check_library()
+
+        self.create_table_with_multilevel_partition()
+        self.alter_leaf_node_to_have_a_different_policy()
+
+        rows = self.db.execute("""
+            select * from find_leaf_partitions_with_mismatching_policies_to_root('myschema');
+        """).fetchall()
+
+        self.assertEqual(len(rows), 0)
+
+    def test_partitions_with_alternate_attribute_indexing_of_distribution_keys_is_not_considered_mismatching(self):
+        self.create_self_contained_schema('myschema')
+        self.use_schema('myschema')
+        self.import_partition_check_library()
+
+        self.create_table_with_multilevel_partition()
+        self.exchange_a_valid_table_into_the_partition_hierarchy_with_alternate_attribute_ordering()
+
+        rows = self.db.execute("""
+            select * from find_leaf_partitions_with_mismatching_policies_to_root('myschema');
+        """).fetchall()
+
+        self.assertEqual(rows, [])
